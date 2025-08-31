0e11ff0393acf1474da350ffb97b592bdfd86936
Guanapi <planeta.chompi18@gmail.com>
2025-08-31 21:27:44 +0200

    added actions modified

--- astro.config.ts
+++ astro.config.ts
@@ -21,7 +21,7 @@ import { expressiveCodeOptions, siteConfig } from "./src/site.config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://guanapi.github.io/"gadd,
+	site: "https://guanapi.github.io/",
 	image: {
 		domains: ["webmention.io"],
 	},
